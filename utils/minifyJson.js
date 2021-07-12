600177148b103b1a45ab612e8bc2ce657ccc65ec
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-07-12 14:31:47 -0600

    v1.1.0 - lint JSON

--- utils/minifyJson.js
+++ utils/minifyJson.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
 
 function minifyJson() {
+    const outputFilePath = process.env.OUTPUT_FILE || './outputMinifiedData.json'
     const file = fs.readFileSync(process.env.INPUT_FILE)
 
     const minifiedJson = JSON.stringify(JSON.parse(file))
-    fs.writeFileSync(process.env.OUTPUT_FILE, minifiedJson)
+    fs.writeFileSync(outputFilePath, minifiedJson)
 
     console.log('File minified!')
 }
